202ff8b1ae2ba911bb81d219c1f7c167c112bd41
awa <w123443212021@126.com>
2024-09-15 09:14:18 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int x = 3;
+int y = 3;
+int x_y[3][3] = { 0 };
+int a[3][3] = { {1,1,1},
+                {1,0,0},
+                {1,0,0} };
+int main()
+{
+
+    for (int x = 0; x < 3; ++x) {
+        for (int y = 0; y < 3; ++y) {
+      
+            std::cout << a[x][y];
+        };
+        printf("\n");
+        
+    };
+    a[0][0] = 0;
+    std::cout << a[0][0];
+    return 0;
+}
